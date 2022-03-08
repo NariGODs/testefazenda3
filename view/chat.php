21a97dbe56f1893dc52c71d4f59c1392e52afdac
Felipe Teles <felipe.teles@outlook.com.br>
2022-03-07 23:43:00 -0300

    PT-BR

--- view/chat.php
+++ view/chat.php
@@ -1,10 +1,10 @@
 <div class="box">
     <div class="show-chat">
-        <i>Loading chat...</i>
+        <i>Carregando...</i>
     </div>
     <form action="#" class="chat">
         <div class="row send-chat" style="padding:15px">
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 box-label">Type Message</div>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 box-label">Digite a mensagem</div>
             <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12">
                 <div class="form-group">
                     <!--<textarea required name="message" no-resize class="form-control message" rows="2" cols="*" placeholder="enter text here">
@@ -18,5 +18,5 @@
         </div>
     </form>
 </div>
-<a href="exit.php">Exit</a>
+<a href="exit.php">Desconectar</a>
 <script src="js/chat.js"></script>
